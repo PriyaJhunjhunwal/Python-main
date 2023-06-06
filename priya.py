ff4a717efbd3f3e4d68bfd1e15b82d8899c59a37
PriyaJhunjhunwal <priya.jhunjhunwal@gmail.com>
2023-06-06 20:16:28 +0530

    Initial commit

---
+++ priya.py
@@ -0,0 +1,11 @@
+first_name = "Priya"
+last_name = "Jhunjhunwal"
+brother_name = "Darsh"
+father_name = "Chandan"
+first_name_2 = "Priya,"
+brother_name_2 = "Darsh and"
+print("My name is", first_name,last_name)
+print("Welcome", first_name)
+print("My brother's name",brother_name)
+print("My father's name is",father_name)
+print("welcome",first_name_2,brother_name_2,father_name,last_name)
